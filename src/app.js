bd95061376a097f6622bdffe1910724d0f7a31ac
larissa <larissaalves1102@gmail.com>
2023-04-24 18:50:16 -0300

    alter port

--- src/app.js
+++ src/app.js
@@ -7,5 +7,6 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
+const port = process.env.PORT || 5000;
 
-app.listen(process.env.PORT, () => console.log(`Servidor rodando na porta ${process.env.PORT}`));
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
